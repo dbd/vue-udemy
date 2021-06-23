62083f57dc999c123ffdd8091b4dab254573f52c
Derek Daniels <dbdaniel@mtu.edu>
2021-06-23 09:25:29 -0400

    s2e8 using data in templates

--- Section 2: Vue Basics/app.js
+++ Section 2: Vue Basics/app.js
@@ -1,5 +1,12 @@
 const app = Vue.createApp({
   // data, functions
+  data() {
+    return {
+      title: "The Final Empire",
+      author: "Brandon Brandonson",
+      age: 45,
+    };
+  },
 });
 
 app.mount("#app");
