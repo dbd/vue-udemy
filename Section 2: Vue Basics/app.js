b38970cc027ab210a8151c89754beb6772335a60
Derek Daniels <dbdaniel@mtu.edu>
2021-06-25 14:54:47 -0400

    s2v12 v-for

--- Section 2: Vue Basics/app.js
+++ Section 2: Vue Basics/app.js
@@ -3,9 +3,23 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Brandonson",
-      age: 45,
+      books: [
+        {
+          title: "The Final Empire",
+          author: "Brandon Brandonson",
+          age: 45,
+        },
+        {
+          title: "The Road to El Dorado",
+          author: "Emp Cusco",
+          age: 45,
+        },
+        {
+          title: "The Empire Strikes Back",
+          author: "Stan Lee",
+          age: 45,
+        },
+      ],
       x: 0,
       y: 0,
     };
