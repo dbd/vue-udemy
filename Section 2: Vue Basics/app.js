9f7ff283f0aa708c0f994f6b0e20501df9307676
Derek Daniels <dbdaniel@mtu.edu>
2021-06-25 14:41:02 -0400

    s2v11 other mouse events

--- Section 2: Vue Basics/app.js
+++ Section 2: Vue Basics/app.js
@@ -6,12 +6,18 @@ const app = Vue.createApp({
       title: "The Final Empire",
       author: "Brandon Brandonson",
       age: 45,
+      x: 0,
+      y: 0,
     };
   },
 
   methods: {
-    toggleShowBooks() {
-      this.showBooks = !this.showBooks;
+    handleEvent(e, n) {
+      console.log(e, n);
+    },
+    handleMouseMove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
