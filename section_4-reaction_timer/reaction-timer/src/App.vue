585e232a29e58fcebaeac84f06df3231571df262
Derek Daniels <dbdaniel@mtu.edu>
2021-06-27 13:34:14 -0400

    s4v32 basic setup for app

--- section_4-reaction_timer/reaction-timer/src/App.vue
+++ section_4-reaction_timer/reaction-timer/src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"></Block>
 </template>
 
 <script>
+import Block from "./components/Block";
+
 export default {
   name: "App",
-  components: {}
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    }
+  }
 };
 </script>
 
---
+++ section_4-reaction_timer/reaction-timer/src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+    Click Me!
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"]
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: green;
+  color: honeydew;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
