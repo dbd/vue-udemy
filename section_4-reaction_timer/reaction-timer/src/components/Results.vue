9bf21a8f213f3b65b151e2d0e6c82b93d89c7e35
Derek Daniels <dbdaniel@mtu.edu>
2021-06-27 14:14:26 -0400

    s4v37 add rankings

--- section_4-reaction_timer/reaction-timer/src/components/Results.vue
+++ section_4-reaction_timer/reaction-timer/src/components/Results.vue
@@ -1,10 +1,25 @@
 <template>
   <p>Reaction Time: {{ this.score }} ms</p>
+  <p>{{ msg }}</p>
 </template>
 
 <script>
 export default {
-  props: ["score"]
+  props: ["score"],
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.msg = "Blazing";
+    } else if (this.score < 650) {
+      this.msg = "Meh.";
+    } else {
+      this.msg = "You slow.";
+    }
+  }
 };
 </script>
 
