17b3c0be3024cd99ba9df8e937a86b8d6301e7ad
Derek Daniels <dbdaniel@mtu.edu>
2021-06-27 16:41:39 -0400

    s5v40 two way data binding

--- section_5-forms/web-form/src/components/SignupForm.vue
+++ section_5-forms/web-form/src/components/SignupForm.vue
@@ -1,12 +1,30 @@
 <template>
   <form>
     <label>Email:</label>
-    <input type="email" required />
+    <input type="email" required v-model="email" />
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "",
+    };
+  },
+};
 </script>
 
 <style>
